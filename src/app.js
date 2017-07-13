bcba3fdfb9f7d3ce865a253c38f62b5777950368
uxster <hello@uxster.nl>
2017-07-13 14:30:51 +0200

    got the post request working

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ full sized version, then display it on the bottom panel.*/
 //REQUIRE ALL THE THINGS
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/../public'));
+app.use('/', bodyParser.urlencoded({ extended: true }));
 
 //GET REQUESTS
 app.get('/', function (req, res) {
@@ -31,6 +33,37 @@ app.get("/getImage", function(req, res) {
 		src: [cat1, cat2, cat3, cat4]
 	});
 });
+
+//POST REQUESTS
+app.post("/like", function(req, res) {
+		fs.readFile(__dirname + '/../resources/likes.json', function (err, data) {
+			if (err) {
+				throw err;
+			}
+
+			var parsed = JSON.parse(data);
+			var cat = "cat" + req.body.nr;
+
+			for (var i = 0; i < parsed.length; i++) {
+				if (cat === parsed[i].name){
+					parsed[i].amount++;
+					var amount = parsed[i].amount;
+				}
+			} 
+
+			var stringified = JSON.stringify(parsed);
+
+			fs.writeFile(__dirname + '/../resources/likes.json', stringified, function (err,data) {
+				if(err) {
+					res.send('error: ' + err);
+				} else {
+					res.send({
+						likes: amount
+					});
+				};
+			});
+		});
+	});
 
 //START LISTENING
 var server = app.listen(3000, function () {
